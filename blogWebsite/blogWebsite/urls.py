3136b5019b3d9fe53a2dc287f13b99edea691c1f
md manzer alam <tooshort9541@gmail.com>
2021-04-11 11:20:31 +0530

    made some changes

--- blogWebsite/blogWebsite/urls.py
+++ blogWebsite/blogWebsite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    # path('',include('blog.urls')),
+    path('',include('blog.urls')),
 ]
