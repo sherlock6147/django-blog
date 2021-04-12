ee7e505f76145ba22f98953faa4e01b72a8cb7ae
md manzer alam <tooshort9541@gmail.com>
2021-04-12 22:02:54 +0530

    fixed time inputs and sorting

--- blogWebsite/blog/views.py
+++ blogWebsite/blog/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import Post
 from django.utils import timezone
+import datetime
 
 def index(request):
     all_posts = Post.objects.order_by('-pub_date').filter(pub_date__lte=timezone.now())
@@ -22,7 +23,15 @@ def create(request):
     if(request.method=='POST'):
         print(request.POST)
         # newPost = Post.objects.update_or_create(request.POST['post_title'],request.POST['post_author'],request.POST['post_text'],request.POST['pub_date'])
-        newPost = Post(post_title=request.POST['post_title'], post_author=request.POST['post_author'],post_text= request.POST['post_text'],pub_date= request.POST['pub_date'])
+        time = timezone.now()
+        date = request.POST['pub_date'].split('-')
+        publishing_time = datetime.datetime(int(date[0]),int(date[1]),int(date[2]),time.hour,time.minute,time.second)
+        newPost = Post(post_title=request.POST['post_title'], post_author=request.POST['post_author'],post_text= request.POST['post_text'],pub_date= publishing_time)
         # Post.objects.create(request.POST['post_title'], request.POST['post_author'], request.POST['post_text'], request.POST['pub_date'])
+        # time = datetime.date()
+        # newPost.pub_date.hour = 4
+        # newPost.pub_date.minute = 24
+        # newPost.pub_date.second = 32
+        print(newPost.pub_date)
         newPost.save()
     return render(request,'blog/create.html')
